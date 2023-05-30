65634c55faa8c1d8c1c29712dd8987b80fd2736b
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-30 12:54:11 +0400

    Quation21

--- src/main/java/Cron_Code/Cron_Code/Schdule.java
+++ src/main/java/Cron_Code/Cron_Code/Schdule.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Schdule {
 
-    @Scheduled(cron = " 0 */5 9-18 * * 5-6 " )
+    @Scheduled(cron = " 0 30 3 * * 1-5 " )
 
 
     public void ScheduledTask(){
