54185278164b6ce06dd18f3c0c3ca1d64cd11fbf
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-30 12:24:36 +0400

    Quation1

--- src/main/java/Cron_Code/Cron_Code/CronCodeApplication.java
+++ src/main/java/Cron_Code/Cron_Code/CronCodeApplication.java
@@ -2,8 +2,10 @@ package Cron_Code.Cron_Code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CronCodeApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/Cron_Code/Cron_Code/Schdule.java
@@ -0,0 +1,17 @@
+package Cron_Code.Cron_Code;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Schdule {
+
+    @Scheduled(cron = " 0 */5 * * * * " )
+
+
+    public void ScheduledTask(){
+        System.out.println("Hello All");
+
+    }
+
+}
